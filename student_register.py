8775ef0d94ac1626046f3edcdce1ee63acacfd70
Riyouk <gostoftheuchiha1@gmail.com>
2025-03-26 12:22:46 +0530

    intial pushing the files to git

--- student_register.py
+++ student_register.py
@@ -2,4 +2,4 @@ def register(name,email_id,passwd):
     register_dic = {"name" : name,"email_id" : email_id,"password" : passwd}
     print(f"the entered info are name {name},email_id {email_id},password {passwd}")
 if __name__ == "__main__" :
-    register("abc","abc@gmail.com","12345")   
+    register('abc','abc@gmail.com','12345')   
--- test_reg.py
+++ test_reg.py
@@ -1,3 +1,3 @@
 from student_register import register
 def test_register():
-    assert register('abc','abc@gmail.com','12345')
+    assert register('name','email_id','passwd') == register('abc','email_id','12345')
