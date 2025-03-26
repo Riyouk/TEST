87e4a01d9e48db895c8021a3671aa68f22544417
Riyouk <gostoftheuchiha1@gmail.com>
2025-03-26 12:18:42 +0530

    intial pushing the files to git

--- test_reg.py
+++ test_reg.py
@@ -1,3 +1,3 @@
 from student_register import register
 def test_register():
-    assert register("name","email_id","passwd") == ("abc","abc@gmail.com","12345")
+    assert register('abc','abc@gmail.com','12345')
